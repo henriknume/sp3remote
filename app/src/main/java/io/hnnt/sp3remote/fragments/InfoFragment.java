11dfc53de235213ab5936fc999cbf0f80c8d68ea
henriknume <henriknume@gmail.com>
2016-05-11 17:28:13 +0200

    infofragment done

--- app/src/main/java/io/hnnt/sp3remote/fragments/InfoFragment.java
+++ app/src/main/java/io/hnnt/sp3remote/fragments/InfoFragment.java
@@ -21,6 +21,7 @@ import android.widget.Toast;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+import org.w3c.dom.Text;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,8 +37,17 @@ public class InfoFragment extends Fragment{
     public static final String TAG_EVENTS = "commandhandler";
     Context fcontext;
     Button infoButton;
-    Button clearButton;
-    TextView infoTextView;
+
+    TextView row0;
+    TextView row1;
+    TextView row2;
+    TextView row3;
+    TextView row4;
+    TextView row5;
+    TextView row6;
+    TextView row7;
+    TextView row8;
+    TextView row9;
 
     public InfoFragment() {
         // Required empty public constructor
@@ -54,11 +64,27 @@ public class InfoFragment extends Fragment{
                              Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView()");
         LinearLayout currentLayout = (LinearLayout) inflater.inflate(R.layout.fragment_info, container, false);
-        infoTextView = (TextView) currentLayout.findViewById(R.id.info_textview);
+        //infoTextView = (TextView) currentLayout.findViewById(R.id.info_textview);
         infoButton = (Button) currentLayout.findViewById(R.id.info_button);
-        clearButton = (Button) currentLayout.findViewById(R.id.clear_button);
 
-        createButtonListeners();
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "infobutton pressed");
+                EventBus.getDefault().post(new CommandEvent("info"));
+            }
+        });
+
+        row0 = (TextView) currentLayout.findViewById(R.id.info_field_0_textview);
+        row1 = (TextView) currentLayout.findViewById(R.id.info_field_1_textview);
+        row2 = (TextView) currentLayout.findViewById(R.id.info_field_2_textview);
+        row3 = (TextView) currentLayout.findViewById(R.id.info_field_3_textview);
+        row4 = (TextView) currentLayout.findViewById(R.id.info_field_4_textview);
+        row5 = (TextView) currentLayout.findViewById(R.id.info_field_5_textview);
+        row6 = (TextView) currentLayout.findViewById(R.id.info_field_6_textview);
+        row7 = (TextView) currentLayout.findViewById(R.id.info_field_7_textview);
+        row8 = (TextView) currentLayout.findViewById(R.id.info_field_8_textview);
+        row9 = (TextView) currentLayout.findViewById(R.id.info_field_9_textview);
         return currentLayout;
     }
 
@@ -89,27 +115,19 @@ public class InfoFragment extends Fragment{
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onInfoEvent(InfoEvent event){
-        infoTextView.append(event.getMorningPosDiff() + "\n");
-        infoTextView.append(event.getSoftwareRelease() + "\n");
-        infoTextView.append(event.getCalcSunPosition() + "\n");
-        infoTextView.append(event.getCurrentPosition() + "\n");
-    }
-
-    private void createButtonListeners(){
-        infoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG_EVENTS, "infobutton pressed");
-                EventBus.getDefault().post(new CommandEvent("info"));
-            }
-        });
-
-        clearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG_EVENTS, "clearbutton pressed");
-                infoTextView.setText("cleared\n");
-            }
-        });
+        row0.setText(event.getSoftwareRelease());
+        row1.setText(event.getCurrentPosition());
+        row2.setText(event.getCalcSunPosition());
+        row3.setText(event.getTiltZeroPosition());
+        row4.setText(event.getSensorZeroPosition());
+        row5.setText(event.getNrOfSunnyDays());
+        row6.setText(event.getLastSunTime());
+        row7.setText(event.getNrOfResets());
+        row8.setText(event.getMorningPosDiff());
+        boolean sun = false;
+        if(event.getSunToday().equals("1")){
+            sun = true;
+        }
+        row9.setText("" + sun);
     }
 }
